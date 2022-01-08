90a70468040c9dedfe8e465e1991b597113ab005
ankitabhisheksahoo <89767668+ankitabhisheksahoo@users.noreply.github.com>
2022-01-08 20:55:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw(){
+  background(0);
+if(keyIsDown(DOWN_ARROW)){
+  background("red");
+}
+if(keyIsDown(UP_ARROW)){
+  background("blue");
+}
+if(keyIsDown(RIGHT_ARROW)){
+  background("yellow");
+
+}
+if(keyIsDown(LEFT_ARROW)){
+  background("brown");
+}
+ 
+}
